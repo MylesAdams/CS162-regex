3f203c93990f6e182428afb79bff857ae6ac6555
Myles Adams <myles.n.adams@gmail.com>
2018-11-03 20:45:55 -0700

    ComputeNext done, and I think ComputeDfa

--- src/main/scala/DerivativeAnalysis.scala
+++ src/main/scala/DerivativeAnalysis.scala
@@ -24,12 +24,50 @@ object DerivativeAnalysis {
   // Private details.
   //----------------------------------------------------------------------------
 
+  implicit class CharSetPairwise(charsets: Set[CharSet]) {
+    def ^(other: Set[CharSet]): Set[CharSet] = {
+      charsets.flatMap(cs => other.foldLeft(Set[CharSet]()){(set, csi) => set + (csi & cs)})
+    }
+  }
+
   // Compute the transitions and set of reachable states (i.e., Regexes) for all
   // Regexes in 'todo'.
   @annotation.tailrec
   private def computeDfa(todo: Set[Regex], visitedStates: Set[Regex],
-    transitions: Transitions[Regex]) : (Set[Regex], Transitions[Regex]) = ???
+                         transitions: Transitions[Regex]) : (Set[Regex], Transitions[Regex]) = {
+    if (!todo.isEmpty) {
+      val x = computeNext(todo.head)
+      computeDfa(todo.drop(1) ++ x._1, visitedStates + todo.head, transitions ++ x._2)
+    }
+    else {
+      (visitedStates, transitions)
+    }
+
+  }
 
   // Compute the transitions and destination states from the given regex.
-  private def computeNext(state: Regex): (Set[Regex], Transitions[Regex]) = ???
+  private def computeNext(state: Regex): (Set[Regex], Transitions[Regex]) = {
+    def partition(re: Regex): Set[CharSet] = re match {
+      case `∅` => Set(α.chars)
+      case `ε` => Set(α.chars)
+      case Chars(s) => Set(s, α.chars & s)
+      case KleeneStar(r) => partition(r)
+      case Complement(r) => partition(r)
+      case Union(r, s) => partition(r) ^ partition(s)
+      case Intersect(r, s) => partition(r) ^ partition(s)
+      case Concatenate(r, s) if (r.nullable == `∅`) => partition(r)
+      case Concatenate(r, s) => partition(r) ^ partition(s)
+    }
+
+    val derivativeMachine = new DerivativeMachine(state)
+
+    val endStatesAndTransitionSeq = partition(state).foldLeft((Set[Regex](), Seq[(CharSet, Regex)]())) {
+      (acc, cs) => {
+        val derivative = derivativeMachine.derive(cs.minValue)
+        (acc._1 + derivative, acc._2 :+ (cs, derivative))
+      }
+    }
+
+    (endStatesAndTransitionSeq._1, Map(state -> endStatesAndTransitionSeq._2))
+  }
 }
